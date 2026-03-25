df77f2cd14b78ceea759d4ef8e1d42f54f12b887
Paul Catlin <paul@ecatlin.com>
2026-03-25 17:03:32 +0000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from "next";
+import { Analytics } from "@vercel/analytics/next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
         <body className="min-h-full flex flex-col">{children}</body>
+        <Analytics />
         </html>
     );
 }
