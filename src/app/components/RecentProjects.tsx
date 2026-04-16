636bdca895fe5f3c4b1562e0565d2e9f86ebe977
Paul Catlin <paul@ecatlin.com>
2026-04-16 09:59:52 +0100

    prevent hanging recent project cards

--- src/app/components/RecentProjects.tsx
+++ src/app/components/RecentProjects.tsx
@@ -1,9 +1,11 @@
 import {sortedProjects} from "@/app/projects";
 import ProjectCard from "@/app/components/ProjectCard";
+import styles from "./RecentProjects.module.css";
 
 export default function RecentProjects() {
     return (
-        <div className="py-5 grid gap-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(310px,1fr))]">
+        <div className={styles.recentProjects +
+            " py-5 grid gap-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(310px,1fr))]"}>
             {sortedProjects(9).map(([slug, project]) => (
                 <ProjectCard key={slug} slug={slug} {...project} />
             ))}
